e6e024d7e9c2af391a1636428beb70544b5836aa
evgenii tatarkin <evgeniitt123@gmail.com>
2023-02-24 15:03:54 -0300

    updated code

--- src/test/java/ParamTests/ParamTest.java
+++ src/test/java/ParamTests/ParamTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 import org.testng.annotations.Parameters;
 
@@ -14,7 +13,7 @@ public class ParamTest {
     WebDriver driver;
     @Test
     @Parameters({"enteringString", "checkingString"})
-    public void testCompareStrings(@Optional("Кто такой кук") String enteringString,@Optional("Кто такой кук") String checkingString) {
+    public void testCompareStrings(String enteringString,String checkingString) {
 System.setProperty("webdriver.chrome.driver", "src/test/java/ParamTests/resourses/chromedriver");
 driver = new ChromeDriver();
 driver.get("https://google.com");
@@ -22,16 +21,11 @@ driver.get("https://google.com");
         search.sendKeys(enteringString);
         WebElement table = driver.findElement(By.xpath("//div[@class='erkvQe']"));
         String a = table.getText();
-        Assert.assertTrue(a.contains(checkingString));
-        driver.quit();
+        if (a.contains(checkingString)) {
+            driver.quit();
+        } else {
+            System.out.println("Такого выражения в предложенном списке не было :" + checkingString);
+            driver.quit();
+        }
     }
-
-
-//    String driver = "src/test/java/ParamTests/resourses/chromedriver";
-//    @Test
-//    @Parameters("searchKey")
-//
-//    public void testLogin(String searchKey) {
-//        System.out.printf("Check login with browser");
-//    }
 }
